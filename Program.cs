3a69bdf701538a18c4b3ce4d1839c811f2b9a677
PolinaLihane <polin@list.ru>
2022-09-15 23:25:00 +0300

    Записала в реадми инфо о проекте

--- Program.cs
+++ Program.cs
@@ -4,11 +4,37 @@
 using static System.Console;
 Clear();
 string[] array = {"Hello","2","World",":)"};
+string[] NewArray=GetNewArray(array);
+PrintArray(NewArray);
 
-void GetNewArray(string array)
+string[] GetNewArray(string[] array)  
 {
-    for (int i = 0; i < array.Length; i++)
+    int newLength=0;
+    for (int i = 0; i < array.Length; i++) //посчитаем кол-во элементов длиной менее 3
     {
-        if
+        if (array[i].Length<=3) newLength++;
     }
+    string[] newAr=new string[newLength];
+    int j=0;
+    for (int i = 0; i < array.Length; i++)  //соберем новый массив
+    {
+        if (array[i].Length<=3) 
+        {
+            newAr[j]=array[i];
+            j++;
+        }
+    }
+    return newAr;
+}
+
+void PrintArray(string[] ar)
+{
+    int k= ar.Length;
+    Write("[");
+    for (int i = 0; i < ar.Length; i++)
+    {
+        Write($"'{ar[i]}'");
+        if(i!=ar.Length-1) Write(",");
+    }
+    Write("]");
 }
